d280d7ce245e76780fc8f366775196bc05f84277
JohnAsare <johnasare7@gmail.com>
2020-06-19 19:06:13 -0400

    Add Python file with docu

---
+++ upLow.py
@@ -0,0 +1,18 @@
+# John Asare
+# Jun 19 2020
+
+
+""" Write a Python function that accepts a string and calculates the number of upper case letters and
+lower case letters.
+Sample String : 'Hello Mr. Rogers, how are you this fine Tuesday?'
+Expected Output :
+No. of Upper case characters : 4
+No. of Lower case Characters : 33 """
+
+
+def up_low(s):
+    pass
+
+
+s = 'Hello Mr. Rogers, how are you this fine Tuesday?'
+up_low(s)
