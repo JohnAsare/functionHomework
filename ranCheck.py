e31cb7fb656e912a0250c6dd1ae6d0f4c4edc5fb
JohnAsare <johnasare7@gmail.com>
2020-06-19 15:38:17 -0400

    Add python file ranCheck

---
+++ ranCheck.py
@@ -0,0 +1,12 @@
+# John Asare
+# Jun 19 2020
+
+
+""" Write a function that checks whether a number is in a given range (inclusive of high and low) """
+
+
+def ran_check(num, low, high):
+    pass
+
+
+ran_check(5, 2, 7)
