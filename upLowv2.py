0ccf50832c649a7a90c1f711c098e9e209af025c
JohnAsare <johnasare7@gmail.com>
2020-06-19 19:31:56 -0400

    Add upLowv2. Use keyword .count to return amount of upper and lower character in a statement

---
+++ upLowv2.py
@@ -0,0 +1,16 @@
+# John Asare
+# Jun 19 2020
+
+
+""" Another way for solving upLow question"""
+
+
+def up_low(statement):
+    for letter in statement:
+        upps = statement.count(letter.isupper())
+        lows = statement.count(letter.islower())
+        print(f'No. of Upper characters are : {upps}')
+        print(f'No. of Lower characters are : {lows}')
+
+s = 'Hello Mr. Rogers, how are you this fine Tuesday?'
+up_low(s)
