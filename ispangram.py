55f1d18345cbb2541c340d53491433f29bc5a9f0
JohnAsare <johnasare7@gmail.com>
2020-06-19 21:23:47 -0400

    Import the string module

---
+++ ispangram.py
@@ -0,0 +1,16 @@
+# John Asare
+# Jun 19 2020
+
+""" Write a Python function to check whether a string is pangram or not.
+Note : Pangrams are words or sentences containing every letter of the alphabet at least once.
+For example : "The quick brown fox jumps over the lazy dog" """
+
+import string
+
+def ispangram(str1, alphabet=string.ascii_lowercase):
+    pass
+
+
+ispangram("The quick brown fox jumps over the lazy dog")
+
+
