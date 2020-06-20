c3b8f3824efcc0bbf4839ae0abf0c38b2b6266f0
JohnAsare <johnasare7@gmail.com>
2020-06-19 21:05:02 -0400

    Add palindrome function and use pass as the method

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+# John Asare
+# Jun 19 2020
+
+""" Write a Python function that checks whether a passed in string is palindrome or not.
+Note: A palindrome is word, phrase, or sequence that reads the same backward as forward,
+e.g., madam or nurses run."""
+
+
+def palindrome(s):
+    print(reversed(s))
+
+palindrome('John')
