65dec163d66b3db0558af223250aeb51232e9699
JohnAsare <johnasare7@gmail.com>
2020-06-19 20:52:50 -0400

    Add python file

---
+++ multiply.py
@@ -0,0 +1,13 @@
+# John Asare
+# Jun 19 2020
+
+""" Write a Python function to multiply all the numbers in a list.
+Sample List : [1, 2, 3, -4]
+Expected Output : -24 """
+
+
+def multiply(numbers):
+    pass
+
+
+multiply([1, 2, 3, -4])
